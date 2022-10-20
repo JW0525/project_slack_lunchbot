fb53d3f8567fff8d68ac5ec252baa2261e98ce45
JW0525 <lemuel0525@gmail.com>
2022-10-20 17:55:52 +0900

    feat: 테이블만 크롭하는 로직 추가

--- app.py
+++ app.py
@@ -8,45 +8,16 @@ import os
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-import function
+
+from function import Recognition
+from imageToXlsx import extractXlsx
+
+src = 'src/images/b93927bd-e957-440d-accc-b4320e29de85.png'
 
 
-src = 'src/images/eaac2f9f-709d-44d0-9c67-9fa70c195de8.png'
-
-result = pytesseract.image_to_string(Image.open(src), lang = "kor")
-
-#print(type(result))
-print(result)
-
-with open('./people.csv', 'w') as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(result)
-
-string = open('./people.csv').read()
-new_str = re.sub('[^a-zA-Z0-9\n\.]', ' ', string)
-open('./people.csv', 'w').write(new_str)
-
-
-
-## 이미지 크기 구하기
-# image1 = Image.open(src)
-# print(image1.size)
-
-
-
-
-
-# f = open('new.txt', 'w')
-# result = function.Recognition().ExtractText('./inversed_image.png')
-# f.write(result)
-# f.close()
-
-
-# result = function.Recognition().DetectImageTable(src)
-
-# image = function.Recognition().crop_to_text('./savefig_default.png')
-# cv2.imwrite("./example-table-cell-1-1-cropped.png", image)
-
+trimmedWhiteSpace = Recognition().trimWhiteSpace(src)
+croppedImage = Recognition().CropImage(trimmedWhiteSpace[0])
+extractXlsx(croppedImage[0])
 
 
 
@@ -58,11 +29,6 @@ open('./people.csv', 'w').write(new_str)
 #         day_list.extend(line.replace('\n', '').split('  '))
 #         day_list = list(filter(None, day_list))
 # f.close()
-#
-#
-#
-#
-#
 #
 # file = pd.read_csv('new.txt', delimiter = '\t')
 # new_csv_file = file.to_csv( './new.csv')
--- function.py
+++ function.py
@@ -1,81 +1,120 @@
-from PIL import Image
+from PIL import Image, ImageChops
 import pytesseract
 import os
 import cv2
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-src = 'src/images/b93927bd-e957-440d-accc-b4320e29de85.png'
 
 class Recognition:
-    ## 글자 추출
+    ## 이미지 여백 제거
+    def trimWhiteSpace(self, src):
+        img = cv2.imread(src) # Read in the image and convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = 255 * (gray < 128).astype(np.uint8) # To invert the text to white
+        coords = cv2.findNonZero(gray) # Find all non-zero points (text)
+        x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
+        rect = img[y:y+h, x:x+w] # Crop the image - note we do this on the original image
+        # cv2.imshow("Cropped", rect) # Show it
+        resize_rect = cv2.resize(rect, (1085, 760)) # 사이즈 통일
+        cv2.imwrite("src/results/trimmed_whiteSpace.png", resize_rect) # Save the image
+        return "src/results/trimmed_whiteSpace.png", resize_rect
+
+    ## 이미지 크롭
+    def CropImage(self, src):
+        image = Image.open(src)
+        size = image.size
+        crop = image.crop((92, 140, size[0], size[1]-100))
+        # crop.show()
+        crop.save('src/results/cropped_image.png')
+        return 'src/results/cropped_image.png', crop'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ##제글자 추출
     def ExtractText(self, src):
-#         img = cv2.imread(src, cv2.IMREAD_COLOR)
-#         copy_img = img.copy()
-#         img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         cv2.imwrite('gray.jpg', img2)
-#         blur = cv2.GaussianBlur(img2, (3,3), 0)
-#         cv2.imwrite('blur.jpg', blur)
-#         canny = cv2.Canny(blur, 100, 200)
-#         cv2.imwrite('canny.jpg', canny)
-#
-#         contours, hierarchy  = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#
-#         box1 = []
-#         f_count = 0
-#         select = 0
-#         plate_width = 0
-#
-#         for i in range(len(contours)):
-#            cnt = contours[i]
-#            area = cv2.contourArea(cnt)
-#            x,y,w,h = cv2.boundingRect(cnt)
-#            rect_area = w * h  #area size
-#            aspect_ratio = float(w)/h # ratio = width/height
-#
-#            if  (aspect_ratio >= 0.2) and (aspect_ratio <= 1.0) and (rect_area >= 100) and (rect_area <= 700):
-#                 cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 1)
-#                 box1.append(cv2.boundingRect(cnt))
-#
-#         for i in range(len(box1)): ##Buble Sort on python
-#            for j in range(len(box1) - (i + 1)):
-#                 if box1[j][0] > box1[j + 1][0]:
-#                      temp = box1[j]
-#                      box1[j] = box1[j + 1]
-#                      box1[j + 1] = temp
-#
-#         #to find number plate measureing length between rectangles
-#         for m in range(len(box1)):
-#            count = 0
-#            for n in range(m + 1,(len(box1) - 1)):
-#                 delta_x = abs(box1[n + 1][0] - box1[m][0])
-#                 if delta_x > 150:
-#                      break
-#                 delta_y = abs(box1[n + 1][1] - box1[m][1])
-#                 if delta_x == 0:
-#                      delta_x = 1
-#                 if delta_y == 0:
-#                      delta_y = 1
-#                 gradient = float(delta_y) / float(delta_x)
-#                 if gradient < 0.25:
-#                     count = count + 1
-#            #measure number plate size
-#            if count > f_count:
-#                 select = m
-#                 f_count = count;
-#                 plate_width=delta_x
-#         cv2.imwrite('snake.jpg', img)
-#
-#         number_plate=copy_img[box1[select][1]-10:box1[select][3]+box1[select][1]+20,box1[select][0]-10:140+box1[select][0]]
-#         resize_plate=cv2.resize(number_plate,None,fx=1.8,fy=1.8,interpolation=cv2.INTER_CUBIC+cv2.INTER_LINEAR)
-#         plate_gray=cv2.cvtColor(resize_plate,cv2.COLOR_BGR2GRAY)
-#         ret,th_plate = cv2.threshold(plate_gray,150,255,cv2.THRESH_BINARY)
-#
-#         cv2.imwrite('plate_th.jpg',th_plate)
-#         kernel = np.ones((3,3),np.uint8)
-#         er_plate = cv2.erode(th_plate,kernel,iterations=1)
-#         er_invplate = er_plate
-#         cv2.imwrite('er_plate.jpg',er_invplate)
+        img = cv2.imread(src, cv2.IMREAD_COLOR)
+        copy_img = img.copy()
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('gray.jpg', img2)
+        blur = cv2.GaussianBlur(img2, (3,3), 0)
+        cv2.imwrite('blur.jpg', blur)
+        canny = cv2.Canny(blur, 100, 200)
+        cv2.imwrite('canny.jpg', canny)
+
+        contours, hierarchy  = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        box1 = []
+        f_count = 0
+        select = 0
+        plate_width = 0
+
+        for i in range(len(contours)):
+           cnt = contours[i]
+           area = cv2.contourArea(cnt)
+           x,y,w,h = cv2.boundingRect(cnt)
+           rect_area = w * h  #area size
+           aspect_ratio = float(w)/h # ratio = width/height
+
+           if  (aspect_ratio >= 0.2) and (aspect_ratio <= 1.0) and (rect_area >= 100) and (rect_area <= 700):
+                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 1)
+                box1.append(cv2.boundingRect(cnt))
+
+        for i in range(len(box1)): ##Buble Sort on python
+           for j in range(len(box1) - (i + 1)):
+                if box1[j][0] > box1[j + 1][0]:
+                     temp = box1[j]
+                     box1[j] = box1[j + 1]
+                     box1[j + 1] = temp
+
+        #to find number plate measureing length between rectangles
+        for m in range(len(box1)):
+           count = 0
+           for n in range(m + 1,(len(box1) - 1)):
+                delta_x = abs(box1[n + 1][0] - box1[m][0])
+                if delta_x > 150:
+                     break
+                delta_y = abs(box1[n + 1][1] - box1[m][1])
+                if delta_x == 0:
+                     delta_x = 1
+                if delta_y == 0:
+                     delta_y = 1
+                gradient = float(delta_y) / float(delta_x)
+                if gradient < 0.25:
+                    count = count + 1
+           #measure number plate size
+           if count > f_count:
+                select = m
+                f_count = count;
+                plate_width=delta_x
+        cv2.imwrite('snake.jpg', img)
+
+        number_plate=copy_img[box1[select][1]-10:box1[select][3]+box1[select][1]+20,box1[select][0]-10:140+box1[select][0]]
+        resize_plate=cv2.resize(number_plate,None,fx=1.8,fy=1.8,interpolation=cv2.INTER_CUBIC+cv2.INTER_LINEAR)
+        plate_gray=cv2.cvtColor(resize_plate,cv2.COLOR_BGR2GRAY)
+        ret,th_plate = cv2.threshold(plate_gray,150,255,cv2.THRESH_BINARY)
+
+        cv2.imwrite('plate_th.jpg',th_plate)
+        kernel = np.ones((3,3),np.uint8)
+        er_plate = cv2.erode(th_plate,kernel,iterations=1)
+        er_invplate = er_plate
+        cv2.imwrite('er_plate.jpg',er_invplate)
 
         result = pytesseract.image_to_string(Image.open(src), lang = 'kor')
         return result
@@ -90,30 +129,32 @@ class Recognition:
                 if name.endswith(format):
                     fullName = os.path.join(root, name)
                     fileList.append(fullName)
+
+        # load the original image
+        myFileList = createFileList(src)
+
+        for file in myFileList:
+            print(file)
+            img_file = Image.open(file)
+
+            # get original image parameters...
+            width, height = img_file.size
+            format = img_file.format
+            mode = img_file.mode
+
+            # Make image Greyscale
+            img_grey = img_file.convert('L')
+
+            # Save Greyscale values
+            value = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
+            value = value.flatten()
+            print(value)
+            with open("img_pixels.csv", 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow(value)
+
         return fileList
 
-    # load the original image
-    myFileList = createFileList(src)
-
-    for file in myFileList:
-        print(file)
-        img_file = Image.open(file)
-
-        # get original image parameters...
-        width, height = img_file.size
-        format = img_file.format
-        mode = img_file.mode
-
-        # Make image Greyscale
-        img_grey = img_file.convert('L')
-
-        # Save Greyscale values
-        value = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
-        value = value.flatten()
-        print(value)
-        with open("img_pixels.csv", 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(value)
 
     ## 테이블 감지
     def DetectImageTable(self, src):
@@ -142,54 +183,53 @@ class Recognition:
         plt.imsave('savefig_default.png', table_image)
         return table_image
 
+    ## Cropping each cell to the text
+    def crop_to_text(self, image):
+        MAX_COLOR_VAL = 255
+        BLOCK_SIZE = 15
+        SUBTRACT_FROM_MEAN = -2
 
-#     ## Cropping each cell to the text
-#     def crop_to_text(self, image):
-#         MAX_COLOR_VAL = 255
-#         BLOCK_SIZE = 15
-#         SUBTRACT_FROM_MEAN = -2
-#
-#         img_bin = cv2.adaptiveThreshold(
-#             image,
-#             MAX_COLOR_VAL,
-#             cv2.ADAPTIVE_THRESH_MEAN_C,
-#             cv2.THRESH_BINARY,
-#             BLOCK_SIZE,
-#             SUBTRACT_FROM_MEAN,
-#         )
-#
-#         img_h, img_w = image.shape
-#         horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (int(img_w * 0.5), 1))
-#         vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, int(img_h * 0.7)))
-#         horizontal_lines = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, horizontal_kernel)
-#         vertical_lines = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, vertical_kernel)
-#         both = horizontal_lines + vertical_lines
-#         cleaned = img_bin - both
-#
-#         # Get rid of little noise.
-#         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-#         opened = cv2.morphologyEx(cleaned, cv2.MORPH_OPEN, kernel)
-#         opened = cv2.dilate(opened, kernel)
-#
-#         contours, hierarchy = cv2.findContours(opened, c v2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-#         bounding_rects = [cv2.boundingRect(c) for c in contours]
-#         NUM_PX_COMMA = 6
-#         MIN_CHAR_AREA = 5 * 9
-#         char_sized_bounding_rects = [(x, y, w, h) for x, y, w, h in bounding_rects if w * h > MIN_CHAR_AREA]
-#         if char_sized_bounding_rects:
-#             minx, miny, maxx, maxy = math.inf, math.inf, 0, 0
-#             for x, y, w, h in char_sized_bounding_rects:
-#                 minx = min(minx, x)
-#                 miny = min(miny, y)
-#                 maxx = max(maxx, x + w)
-#                 maxy = max(maxy, y + h)
-#             x, y, w, h = minx, miny, maxx - minx, maxy - miny
-#             cropped = image[y:min(img_h, y+h+NUM_PX_COMMA), x:min(img_w, x+w)]
-#         else:
-#             # If we morphed out all of the text, assume an empty image.
-#             cropped = MAX_COLOR_VAL * np.ones(shape=(20, 100), dtype=np.uint8)
-#         bordered = cv2.copyMakeBorder(cropped, 5, 5, 5, 5, cv2.BORDER_CONSTANT, None, 255)
-#         return bordered
+        img_bin = cv2.adaptiveThreshold(
+            image,
+            MAX_COLOR_VAL,
+            cv2.ADAPTIVE_THRESH_MEAN_C,
+            cv2.THRESH_BINARY,
+            BLOCK_SIZE,
+            SUBTRACT_FROM_MEAN,
+        )
+
+        img_h, img_w = image.shape
+        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (int(img_w * 0.5), 1))
+        vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, int(img_h * 0.7)))
+        horizontal_lines = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, horizontal_kernel)
+        vertical_lines = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, vertical_kernel)
+        both = horizontal_lines + vertical_lines
+        cleaned = img_bin - both
+
+        # Get rid of little noise.
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        opened = cv2.morphologyEx(cleaned, cv2.MORPH_OPEN, kernel)
+        opened = cv2.dilate(opened, kernel)
+
+        contours, hierarchy = cv2.findContours(opened, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        bounding_rects = [cv2.boundingRect(c) for c in contours]
+        NUM_PX_COMMA = 6
+        MIN_CHAR_AREA = 5 * 9
+        char_sized_bounding_rects = [(x, y, w, h) for x, y, w, h in bounding_rects if w * h > MIN_CHAR_AREA]
+        if char_sized_bounding_rects:
+            minx, miny, maxx, maxy = math.inf, math.inf, 0, 0
+            for x, y, w, h in char_sized_bounding_rects:
+                minx = min(minx, x)
+                miny = min(miny, y)
+                maxx = max(maxx, x + w)
+                maxy = max(maxy, y + h)
+            x, y, w, h = minx, miny, maxx - minx, maxy - miny
+            cropped = image[y:min(img_h, y+h+NUM_PX_COMMA), x:min(img_w, x+w)]
+        else:
+            # If we morphed out all of the text, assume an empty image.
+            cropped = MAX_COLOR_VAL * np.ones(shape=(20, 100), dtype=np.uint8)
+        bordered = cv2.copyMakeBorder(cropped, 5, 5, 5, 5, cv2.BORDER_CONSTANT, None, 255)
+        return bordered
 
 
 
